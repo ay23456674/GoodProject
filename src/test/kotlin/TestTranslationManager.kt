4b412837913c4d371b8df3f100890acf05a9401d
Rafael Stein Matsumoto <rafaelmatsumoto43@gmail.com>
2020-10-12 14:35:10 -0300

    test: improve project tests

--- src/test/kotlin/TestTranslationManager.kt
+++ src/test/kotlin/TestTranslationManager.kt
@@ -11,5 +11,6 @@ class TestTranslationManager {
     fun translationManager() {
         val translationManager = TranslationManager()
         assert("Translate (Some text) from English to German" == translationManager.translate("Some text", Language.English, Language.German))
+        assert("Translate (überraschung) from German to English" == translationManager.translate("überraschung", Language.German, Language.English))
     }
 }
